968910326d9feee9af89bbabad24281975cd6a09
DevAlexeyProtasenya <37490289+DevAlexeyProtasenya@users.noreply.github.com>
2021-09-21 15:37:23 +0300

    feat: add handlers

---
+++ common/common.js
@@ -0,0 +1,13 @@
+var ToastrType = {
+  ERROR: 'error',
+  WARNING: 'warning',
+  SUCCESS: 'success',
+  INFO: 'info',
+}
+
+var ToastrImages = {
+  ERROR: './public/toastIcons/error.svg',
+  WARNING: './public/toastIcons/warning.svg',
+  SUCCESS: './public/toastIcons/success.svg',
+  INFO: './public/toastIcons/info.svg',
+}
