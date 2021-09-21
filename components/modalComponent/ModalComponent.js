c4b152cd4841ee20f35086bbe8c8eb27a1e1232a
DevAlexeyProtasenya <37490289+DevAlexeyProtasenya@users.noreply.github.com>
2021-09-21 15:30:34 +0300

    feat: add Modal component

---
+++ components/modalComponent/ModalComponent.js
@@ -0,0 +1,30 @@
+function Modal (content){
+  Popup.call(this, 'div', 'body');
+  this.content = content;
+  this.modal = this.createModal();
+  this.createElement();
+}
+
+Modal.prototype = Object.create(Popup.prototype);
+Modal.prototype.constructor = Modal;
+
+Modal.prototype.createElement = function(){
+  this.addClassesToElement(this.element, ['modal-wrapper']);
+  this.addListener(this.element, 'click', this.closeModal);
+  this.addElement(this.modal);
+}
+
+Modal.prototype.createModal = function(){
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+  if(this.content){
+    modal.appendChild(this.content);
+  }
+  return modal;
+}
+
+Modal.prototype.closeModal = function(event){
+  if(event.target.classList.contains('modal-wrapper')){
+    this.hide();
+  }
+}
